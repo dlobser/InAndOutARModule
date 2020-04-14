594919df8f855a34b9eb636d63655616462e1095
david lobser <davidlobser@yahoo.com>
2020-04-14 09:04:06 -0600

    added touch to spin globe example

---
+++ Class_02/TouchToSpinGlobe/touchAndRotate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchAndRotate : MonoBehaviour
+{
+    public GameObject globe;
+    GameObject aimer;
+
+    void Start()
+    {
+        aimer = new GameObject();
+    }
+
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                aimer.transform.LookAt(hit.point);
+                globe.transform.parent = aimer.transform;
+            }
+            if (Input.GetMouseButton(0))
+            {
+                aimer.transform.LookAt(hit.point);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                globe.transform.parent = null;
+            }
+
+        }
+    }
+}
